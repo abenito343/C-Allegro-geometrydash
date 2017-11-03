5db8f343a091e5f55dfaf25faf90ec54bdbd6110
arixelbenito343 <abenito@est.frba.utn.edu.ar>
2017-11-02 23:51:07 -0300

    mas trabajo con sprites

--- GeometryDash.c
+++ GeometryDash.c
@@ -14,7 +14,7 @@ const int BOUNCER_SIZE = 32;
 
 enum MYKEYS {
 
-   KEY_SPACE
+   KEY_UP,KEY_SPACE
 };
  
 int main(int argc, char **argv){
@@ -30,6 +30,7 @@ int main(int argc, char **argv){
    ALLEGRO_BITMAP  *image3   = NULL;//cubo
    ALLEGRO_BITMAP  *image4   = NULL;//enemigo
    ALLEGRO_BITMAP  *image5   = NULL;//explosion
+   ALLEGRO_BITMAP  *image6   = NULL;//explosion
    
 	const int maxFrame = 10;
 	int curFrame = 0;
@@ -50,13 +51,17 @@ int main(int argc, char **argv){
    float bouncer_x4 = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
    float bouncer_y4 = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;//fondo
    
+   
+   float bouncer_x5 = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+   float bouncer_y5 = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;//materiales
+   
    float bouncer_dx = -4.0, bouncer_dy = 4.0;
   
-   float i,i2,i3;
+   float i,i2,i3,i4,i5=0,i6=0,i7=0;
    int aux1, aux2;
    
    bool redraw = true;
-   bool key[1] = { false };
+   bool key[2] = { false , false};
    bool doexit = false;
  
    if(!al_init()) {
@@ -90,7 +95,7 @@ int main(int argc, char **argv){
       return 0;
    }
    
-   image3 = al_load_bitmap("cubo.png");
+   image3 = al_load_bitmap("cubos.png");
    if(!image3) {
       al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
@@ -140,6 +145,7 @@ int main(int argc, char **argv){
    image2 = al_load_bitmap("piso.png");
    image4 = al_load_bitmap("enemigo.png"); 
    image5 = al_load_bitmap("sprite_explosiones.png");
+   image6 = al_load_bitmap("pared.png");
    al_convert_mask_to_alpha(image5, al_map_rgb(106, 76, 48));
  
    if(!image) {
@@ -160,7 +166,29 @@ int main(int argc, char **argv){
       if(ev.type == ALLEGRO_EVENT_TIMER) {
 		  
 		  
-		  //-----------------------------------------------------	  	  
+		  //-----------------------------------------------------	
+		  
+		  
+		  
+		  if(key[KEY_UP] ) {
+			 i6=i6+1;
+			 if(i6==3){
+				i5=i5+1;
+				i6=12;	
+				}
+		 }  	  
+		 else{
+			 i6=0;
+			 
+		 }
+		 if(i5==12)
+		 {
+			 i5=0;
+			 i7=i7+1;
+		 }
+		 if(i7==5)
+			i7=0;
+		 
 		 if(key[KEY_SPACE] ) {
 			 if( bouncer_y2 >= 300.0){
 				aux1=1;
@@ -188,6 +216,13 @@ int main(int argc, char **argv){
 			}   
 		  }
   		  //-------------------------------------------------
+  		  
+  		  
+  		  
+  		  
+  		  
+		 
+		 //---------------------------------------------------
 		  
 		 	  
          if(bouncer_x < -256)              
@@ -201,8 +236,13 @@ int main(int argc, char **argv){
                         
             
          if(bouncer_x3 < -256)                
-			bouncer_x3= bouncer_x+1256;
+			bouncer_x3= bouncer_x3+1256;
          bouncer_x3 += bouncer_dx*2;
+         
+         
+         if(bouncer_x5 < -956)                
+			bouncer_x5= bouncer_x5+2056;
+         bouncer_x5 += bouncer_dx;
            
            //---------------------------------------------------- 
             
@@ -230,6 +270,9 @@ int main(int argc, char **argv){
       else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
          switch(ev.keyboard.keycode) {
 			 
+			   case ALLEGRO_KEY_UP:
+               key[KEY_UP] = true;
+               break;
 			 case ALLEGRO_KEY_SPACE:
                key[KEY_SPACE] = true;
                break;
@@ -237,6 +280,10 @@ int main(int argc, char **argv){
       }
       else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
          switch(ev.keyboard.keycode) {
+			 
+			   case ALLEGRO_KEY_UP:
+               key[KEY_UP] = false;
+               break;
 			 
 			 case ALLEGRO_KEY_SPACE:
               key[KEY_SPACE] = false;
@@ -249,6 +296,7 @@ int main(int argc, char **argv){
 		
 		i=bouncer_x;
 		i2=bouncer_x4;
+		
 
 		
 	    al_clear_to_color(al_map_rgb(0,0,0));
@@ -265,14 +313,31 @@ int main(int argc, char **argv){
 	   al_draw_bitmap(image2,i+256*4,500,0);
 	   al_draw_bitmap(image2,i+256*5,500,0);
 	   
-	   al_draw_bitmap(image4,bouncer_x3,350,0);
+	   al_draw_bitmap(image6,bouncer_x5+400,440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*1),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*2),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*3),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*4),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*5),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*6),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*7),440,100);
+	   al_draw_bitmap(image6,bouncer_x5+400+(62*8),440,100);
+	 
 	   
-	   al_draw_bitmap(image3,bouncer_x2-200, bouncer_y2+115,0);
+	 //  al_draw_bitmap(image4,bouncer_x3,350,0);
+	   
+	   	al_draw_bitmap_region(image4,i4*140,0,140,150,bouncer_x3,355,0);
+	   	
+	   	
+	   	al_draw_bitmap_region(image3,9+(97*i5)-(i5/4),55+(96*i7),89,87,bouncer_x2-200, bouncer_y2+109,0);
+	   //al_draw_bitmap(image3,bouncer_x2-200, bouncer_y2+115,0);
 	   
 	    if(bouncer_x3<bouncer_x2-125&&bouncer_x3>bouncer_x2-330){	   
 		   if(bouncer_y2>150){   
 			   curFrame==0;			      	
 				i3=1;
+				i4=0;
+			
 			}
 		}
 	   if(i3==1){
@@ -282,6 +347,7 @@ int main(int argc, char **argv){
        {   
 		   curFrame==0;
 		   i3=0;
+		   i4=1;
 		   }
          al_flip_display();
          
@@ -300,6 +366,7 @@ int main(int argc, char **argv){
    al_destroy_bitmap(image3);
    al_destroy_bitmap(image4);
    al_destroy_bitmap(image5);
+   al_destroy_bitmap(image6);
  
    return 0;
 }
