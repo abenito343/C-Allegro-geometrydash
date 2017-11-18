eacc8aeec070400b23e87a0c6b828a84a230add6
abenito343 <abenito@est.frba.utn.edu.ar>
2017-11-18 12:05:20 -0300

    Merge branch 'master' of https://github.com/abenito343/geometrydash17
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- GeometryDash.h
+++ GeometryDash.h
@@ -1,8 +1,15 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <allegro5/allegro.h>
 #include "allegro5/allegro.h"
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_native_dialog.h"
+#include "allegro5/allegro_font.h"
+#include "allegro5/allegro_ttf.h"
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
 
 
