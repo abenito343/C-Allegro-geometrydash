6d14157757b029f7b6165a8c0c5265dddb866e3e
abenito343 <abenito@est.frba.utn.edu.ar>
2018-03-09 16:04:38 -0300

    Eliminación de imagenes, sonidos y archivos sin utilizar

--- src_cl/Inicializarcl.c
+++ src_cl/Inicializarcl.c
@@ -307,8 +307,8 @@ int	Inicializar (ini_var **var, auxpartida **axpartida, frameExplosion **fEx, fr
 	(variables -> enemigoimg[3])  = al_load_bitmap("imagenes/enemigo3.png"); 
 	(variables -> enemigoimg[4])  = al_load_bitmap("imagenes/enemigo42.jpg");
 	(variables -> enemigoimg[5])  = al_load_bitmap("imagenes/enemigo52.jpg");   
-	(variables -> enemigoimg[6])  = al_load_bitmap("imagenes/Modelo-Estalactita_de_Hielo2.png");
-	(variables -> enemigoimg[7])  = al_load_bitmap("imagenes/index.png");
+	(variables -> enemigoimg[6])  = al_load_bitmap("imagenes/pinchos.png");
+	(variables -> enemigoimg[7])  = al_load_bitmap("imagenes/bala.png");
 				
 	
 	
